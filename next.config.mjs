e2c4ce251652d6a6593b226fbcb0a80c15ec2ea6
Danial Sadiq Masood <danialsadiqmasood44@gmail.com>
2026-02-19 18:50:21 +0500

    add basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // if using static export
+  basePath: "/hexabax-fe",
+  assetPrefix: "/hexabax-fe/",
+  images: {
+    unoptimized: true, // IMPORTANT for GitHub Pages
+  },
+};
 
 export default nextConfig;
